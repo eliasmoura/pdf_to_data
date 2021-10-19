f3dd1432f1e979ccbc8bfcddc01a2af15bca49a3
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-10-19 18:54:09 +0100

    Changed  `main.go` to `pdf_to_ledger.go`.

---
+++ pdf_to_ledger.go
@@ -0,0 +1,39 @@
+package pdf_to_ledger
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	pdf_parser "pdf_to_ledger/lib/pdf"
+	"strings"
+)
+
+func usage(progname string) {
+	fmt.Printf(`%s usage:
+    %s file.pdf [output.txt]
+    `, progname, progname)
+}
+
+func main() {
+	progname := os.Args[0]
+	progname_ := strings.Split(progname, "/")
+	progname = progname_[len(progname_)-1]
+	var filepath string
+	if len(os.Args[1:]) < 1 {
+		usage(progname)
+		filepath = "bank_recipe.pdf"
+	} else {
+		filepath = os.Args[1]
+	}
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	pdf, err := pdf_parser.Parse(file)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	pdf_parser.Print_objs(pdf)
+}
