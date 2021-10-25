8b1ba66c39b5038c48d91fb7c74fd5e61d9f6bb6
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-10-25 10:18:55 +0100

    query: Partial implementation of the query routine.
    This PR implements query parsing, running and outputing it to the user.

--- cmd/pdf_to_data.go
+++ cmd/pdf_to_data.go
@@ -5,8 +5,13 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+<<<<<<< HEAD:cmd/pdf_to_data.go
 	pdf_parser "pdf_to_data/lib/pdf"
 	"pdf_to_data/lib/query"
+=======
+	pdf_parser "pdf_to_ledger/lib/pdf"
+	"pdf_to_ledger/lib/query"
+>>>>>>> 6383949 (query: Partial implementation of the query routine.):pdf_to_ledger.go
 	"strings"
 )
 
