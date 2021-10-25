2b5e4bba0b6686e6919a1185ab8e7bf66222b8eb
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-10-25 10:59:16 +0100

    project: files renamed on the change of the project scope.

--- cmd/pdf_to_data.go
+++ cmd/pdf_to_data.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+<<<<<<< HEAD
 <<<<<<< HEAD:cmd/pdf_to_data.go
 <<<<<<< HEAD:cmd/pdf_to_data.go
 	pdf_parser "pdf_to_data/lib/pdf"
@@ -17,6 +18,10 @@ import (
 	pdf_parser "pdf_to_data/lib/pdf"
 	"pdf_to_data/lib/query"
 >>>>>>> 23e5ab2 (project: change the project scope.):pdf_to_ledger.go
+=======
+	pdf_parser "pdf_to_data/lib/pdf"
+	"pdf_to_data/lib/query"
+>>>>>>> a212228 (project: files renamed on the change of the project scope.)
 	"strings"
 )
 
